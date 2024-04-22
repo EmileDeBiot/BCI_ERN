09390270b3bfb9e6479bfa19239625e5afe9a64d
EmileDeBiot <115916424+EmileDeBiot@users.noreply.github.com>
2024-04-22 15:12:33 +0200

    :art: renaming files and separating methods

--- utils/init_bci_lab.m
+++ utils/init_bci_lab.m
@@ -1,4 +1,4 @@
-function init_bci()
+function init_bci_lab()
     % init_bci: Load BCILAB if it is not already loaded
     if isempty(which('bci_train.m'))
         data_path = 'data/data/';
@@ -6,7 +6,7 @@ function init_bci()
         %cd BCILAB-master/BCILAB-master;
         cd BCILAB;
         bcilab('data',data_path,'menu',false,'mem_capacity',0.5);
-        cd ..
+        cd ../;
         disp('BCILAB loaded!')
     else 
         disp('BCILAB already loaded!')
