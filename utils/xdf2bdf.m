0a76393a5e0e5e6755c76b615d0cd99479adfc05
EmileDeBiot <115916424+EmileDeBiot@users.noreply.github.com>
2024-04-23 16:25:28 +0200

    :sparkles::construction: adding a way to transform xdf to bdf, but there is still a problem with the trigger channel

---
+++ utils/xdf2bdf.m
@@ -0,0 +1,17 @@
+% Load xdf files and transform them to bdf
+
+function xdf2bdf(xdf_file)
+    data_path = 'data/data/';
+    result_path = 'data/results/';
+    eeg = eeg_load_xdf(strcat(data_path,xdf_file));
+    labels = {};
+    for i = 2:73
+        labels = [labels, {eeg.chanlocs(i).labels}];
+    end
+    eeg_data = eeg.data(2:73, :);
+    trigger_data = eeg.data(1, :);
+    filename = strcat(result_path, xdf_file(1:-4), '.bdf');
+    f = writeeeg(filename, eeg_data, eeg.srate, 'label', labels, 'TYPE', 'BDF');
+    t = writeeeg(strcat(filename, '_trigger.bdf'), trigger_data, eeg.srate, 'label', {'Trigger'}, 'TYPE', 'BDF');
+end
+
